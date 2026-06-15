47389997cd8c45596341904833579535d9714e7c
Michael Guldborg <michael.kaaberbolguldborg@resurs.se>
2026-06-15 21:04:08 +0200

    chore: update projects with expo workout app

--- src/models/projects.ts
+++ src/models/projects.ts
@@ -1,6 +1,6 @@
 import Project from "./project";
 
-// orange 
+// orange
 // linear-gradient(45deg,#fbb13c,#ff4e50)
 // pink
 // linear-gradient(45deg, #fad0c4, #ffd1ff)
@@ -9,77 +9,96 @@ import Project from "./project";
 // orange
 // linear-gradient(45deg, #ff4e50, #f9d423)
 
-export const projects: Project[] = [{
-    name: 'Complex Figure Test',
-    company: 'MSc Thesis',
-    role: 'Fullstack/Flutter Developer',
-    url: 'https://github.com/MichaelGuldborg/complex-figure-test',
-    from: '2022',
-    to: '2022',
-    color: '#4b6cb7',
-    gradient: 'linear-gradient(45deg, #284858, #5b7cc7)',
-}, {
-    name: 'Stronglift Clone',
-    company: 'Private',
-    role: 'Fullstack/Flutter Developer',
-    url: 'https://github.com/MichaelGuldborg/stronglift-clone',
-    from: '2022',
-    to: '2022',
-    color: '#f44336',
-    gradient: 'linear-gradient(45deg, #aa2e25, #f6685e)',
-}, {
-    name: 'City Surfing',
-    company: 'Private',
-    role: 'Flutter Developer',
-    url: 'https://github.com/MichaelGuldborg/michaelguldborg.dk/tree/master/public/projects/citysurfing',
-    from: '2021',
-    to: 'now',
-    color: '#57c4a3',
-    gradient: 'linear-gradient(45deg, #4697a1, #68dea0)',
-}, {
-    name: 'Achievement',
-    company: 'Private',
-    role: 'Fullstack/React Developer',
-    url: 'https://github.com/MichaelGuldborg/michaelguldborg.dk/tree/master/public/projects/achivement',
-    from: '2021',
-    to: '2021',
-    color: '#ffaf7b',
-    gradient: 'linear-gradient(45deg, #d76d77, #ffaf7b)'
-},{
-    name: 'Calendar Time',
-    company: 'Private',
-    role: 'React Developer',
-    url: 'https://github.com/MichaelGuldborg/CalendarTime',
-    from: '2021',
-    to: '2021',
-    color: '#43c6ac',
-    gradient: 'linear-gradient(45deg, #43c6ac, #c8cf5e)',
-}, {
-    name: 'Digitalt Tilgængelig',
-    company: 'InnoSocial ApS',
-    role: 'Fullstack/React Developer',
-    url: 'https://github.com/MichaelGuldborg/michaelguldborg.dk/tree/master/public/projects/diti',
-    from: '2021',
-    to: '2021',
-    color: '#087ee1',
-    gradient: 'linear-gradient(45deg,#087ee1,#00caa1)',
-}, {
-    name: 'Headspace Admin',
-    company: 'InnoSocial ApS',
-    role: 'Fullstack/React Developer',
-    url: 'https://github.com/MichaelGuldborg/michaelguldborg.dk/tree/master/public/projects/headspace_admin',
-    from: '2019',
-    to: '2021',
-    color: '#26A95E',
-    gradient: 'linear-gradient(45deg, #06893E, #3CD67C)'
-}, {
-    name: 'MinEjendom',
-    company: 'MinEjendom ApS',
-    role: 'Fullstack/Flutter Developer',
-    url: 'https://github.com/MichaelGuldborg/michaelguldborg.dk/tree/master/public/projects/minejendom',
-    from: '2018',
-    to: 'now',
-    color: '#0053cf',
-    gradient: 'linear-gradient(45deg, #0053cf, #00A3FF)',
-}];
+export const projects: Project[] = [
+  {
+    name: "Workout With Friends",
+    company: "Private",
+    role: "Fullstack / React Native expo Developer",
+    url: "https://workout.michaelguldborg.dk/public/app",
+    from: "2025",
+    to: "2026",
+    color: "#0AB799",
+    gradient: "linear-gradient(45deg, #44d5ba, #008a74)",
+  },
+  {
+    name: "Complex Figure Test",
+    company: "MSc Thesis",
+    role: "Fullstack/Flutter Developer",
+    url: "https://github.com/MichaelGuldborg/complex-figure-test",
+    from: "2022",
+    to: "2022",
+    color: "#4b6cb7",
+    gradient: "linear-gradient(45deg, #284858, #5b7cc7)",
+  },
+  {
+    name: "Stronglift Clone",
+    company: "Private",
+    role: "Fullstack/Flutter Developer",
+    url: "https://github.com/MichaelGuldborg/stronglift-clone",
+    from: "2022",
+    to: "2022",
+    color: "#f44336",
+    gradient: "linear-gradient(45deg, #aa2e25, #f6685e)",
+  },
+  {
+    name: "City Surfing",
+    company: "Private",
+    role: "Flutter Developer",
+    url: "https://github.com/MichaelGuldborg/michaelguldborg.dk/tree/master/public/projects/citysurfing",
+    from: "2021",
+    to: "2021",
+    color: "#57c4a3",
+    gradient: "linear-gradient(45deg, #4697a1, #68dea0)",
+  },
+  {
+    name: "Achievement",
+    company: "Private",
+    role: "Fullstack/React Developer",
+    url: "https://github.com/MichaelGuldborg/michaelguldborg.dk/tree/master/public/projects/achivement",
+    from: "2021",
+    to: "2021",
+    color: "#ffaf7b",
+    gradient: "linear-gradient(45deg, #d76d77, #ffaf7b)",
+  },
+  {
+    name: "Calendar Time",
+    company: "Private",
+    role: "React Developer",
+    url: "https://github.com/MichaelGuldborg/CalendarTime",
+    from: "2021",
+    to: "2021",
+    color: "#43c6ac",
+    gradient: "linear-gradient(45deg, #43c6ac, #c8cf5e)",
+  },
+  {
+    name: "Digitalt Tilgængelig",
+    company: "InnoSocial ApS",
+    role: "Fullstack/React Developer",
+    url: "https://github.com/MichaelGuldborg/michaelguldborg.dk/tree/master/public/projects/diti",
+    from: "2021",
+    to: "2021",
+    color: "#087ee1",
+    gradient: "linear-gradient(45deg,#087ee1,#00caa1)",
+  },
+  {
+    name: "Headspace Admin",
+    company: "InnoSocial ApS",
+    role: "Fullstack/React Developer",
+    url: "https://github.com/MichaelGuldborg/michaelguldborg.dk/tree/master/public/projects/headspace_admin",
+    from: "2019",
+    to: "2021",
+    color: "#26A95E",
+    gradient: "linear-gradient(45deg, #06893E, #3CD67C)",
+  },
+  {
+    name: "MinEjendom",
+    company: "MinEjendom ApS",
+    role: "Fullstack/Flutter Developer",
+    url: "https://github.com/MichaelGuldborg/michaelguldborg.dk/tree/master/public/projects/minejendom",
+    from: "2018",
+    to: "2020",
+    color: "#0053cf",
+    gradient: "linear-gradient(45deg, #0053cf, #00A3FF)",
+  },
+];
 export default projects;
